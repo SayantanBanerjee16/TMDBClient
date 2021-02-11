f77a66837ca4b2ae64b5e10a3c11625511a21d84
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2021-02-11 14:22:05 +0530

    Defined the Retrofit Instance

---
+++ app/src/main/java/com/sayantanbanerjee/tmdbclient/data/api/TMDBService.kt
@@ -0,0 +1,25 @@
+package com.sayantanbanerjee.tmdbclient.data.api
+
+import com.sayantanbanerjee.tmdbclient.data.ArtistList
+import com.sayantanbanerjee.tmdbclient.data.MovieList
+import com.sayantanbanerjee.tmdbclient.data.TvShowList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// Retrofit Instance which define the endpoints through which data is fetched from the API.
+interface TMDBService {
+
+    // Fetching the popular Movies.
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String): Response<MovieList>
+
+    // Fetching the popular TV shows.
+    @GET("tv/popular")
+    suspend fun getPopularTvShows(@Query("api_key") apiKey: String): Response<TvShowList>
+
+    // Fetching the popular Artists.
+    @GET("person/popular")
+    suspend fun getPopularArtists(@Query("api_key") apiKey: String): Response<ArtistList>
+
+}
